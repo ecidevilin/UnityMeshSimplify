e0979a26bf47ff9c262593061cfacdd22dc678b2
Chaos <ecidevilin@live.cn>
2018-07-17 19:35:45 +0800

    change folder

---
+++ MeshSimplify/Scripts/Util/UnsafeUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chaos
+{
+
+    public static class UnsafeUtil
+    {
+        public static float UintToFloat(uint u)
+        {
+            unsafe
+            {
+                return *(float*)&u;
+            }
+        }
+
+        public static uint FloatToUint(float f)
+        {
+            unsafe
+            {
+                return *(uint*) &f;
+            }
+        }
+    }
+
+
+}
